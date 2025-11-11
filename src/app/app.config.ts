8c297b79df1d2fea9ff98b78ef1682390a4c9117
ckab04 <ciceronkapenabm@gmail.com>
2025-11-11 12:58:59 +0100

    Quelques configurations pour faire tourner l'application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,35 +2,33 @@ import {
   ApplicationConfig,
   isDevMode,
   provideZoneChangeDetection,
-} from '@angular/core'
+} from "@angular/core";
 import {
   provideRouter,
   withInMemoryScrolling,
   type InMemoryScrollingFeature,
   type InMemoryScrollingOptions,
-} from '@angular/router'
-import { provideStoreDevtools } from '@ngrx/store-devtools'
-import { routes } from './app.routes'
-import { provideEffects } from '@ngrx/effects'
-import { DatePipe, DecimalPipe } from '@angular/common'
-import { AuthenticationEffects } from './store/authentication/authentication.effects'
-import {
-  provideHttpClient,
-  withInterceptors
-} from '@angular/common/http'
-import { FakeBackendProvider } from './core/helpers/fake-backend'
-import { CalendarEffects } from './store/calendar/calendar.effects'
-import { KanbanEffects } from './store/kanban/kanban.effects'
-import { authInterceptor } from './auth/auth.interceptor'
+} from "@angular/router";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+import { routes } from "./app.routes";
+import { provideEffects } from "@ngrx/effects";
+import { DatePipe, DecimalPipe } from "@angular/common";
+import { AuthenticationEffects } from "./store/authentication/authentication.effects";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { FakeBackendProvider } from "./core/helpers/fake-backend";
+import { CalendarEffects } from "./store/calendar/calendar.effects";
+import { KanbanEffects } from "./store/kanban/kanban.effects";
+import { authInterceptor } from "./auth/auth.interceptor";
+import { provideStore } from "@ngrx/store";
 
 // scroll
 const scrollConfig: InMemoryScrollingOptions = {
-  scrollPositionRestoration: 'top',
-  anchorScrolling: 'enabled',
-}
+  scrollPositionRestoration: "top",
+  anchorScrolling: "enabled",
+};
 
 const inMemoryScrollingFeatures: InMemoryScrollingFeature =
-  withInMemoryScrolling(scrollConfig)
+  withInMemoryScrolling(scrollConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -45,7 +43,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, inMemoryScrollingFeatures),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideEffects(CalendarEffects),
-    provideEffects(AuthenticationEffects,KanbanEffects),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideEffects(AuthenticationEffects, KanbanEffects),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideStore({}),
   ],
-}
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,61 @@
-import { RedirectCommand, Router, Routes, UrlTree } from '@angular/router'
-import { LayoutComponent } from './layouts/layout/layout.component'
-import { Error404Component } from './views/auth/error404/error404.component'
-import { Error500Component } from './views/auth/error500/error500.component'
-import { MaintenanceComponent } from './views/auth/maintenance/maintenance.component'
-import { inject } from '@angular/core'
-import { AuthenticationService } from './core/service/auth.service'
-import { authGuard } from './core/service/auth.guard'
+import { RedirectCommand, Router, Routes, UrlTree } from "@angular/router";
+import { LayoutComponent } from "./layouts/layout/layout.component";
+import { Error404Component } from "./views/auth/error404/error404.component";
+import { Error500Component } from "./views/auth/error500/error500.component";
+import { MaintenanceComponent } from "./views/auth/maintenance/maintenance.component";
+import { inject } from "@angular/core";
+import { AuthenticationService } from "./core/service/auth.service";
+import { authGuard } from "./core/service/auth.guard";
+import { BlogsComponent } from "./views/pages/blogs/blogs.component";
+import { ProjectComponent } from "./views/applications/projects/project/project.component";
+import { ClientsComponent } from "./views/applications/projects/clients/clients.component";
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard/analytics',
-    pathMatch: 'full',
+    path: "",
+    component: BlogsComponent,
+    pathMatch: "full",
   },
   {
-    path: '',
+    path: "projects",
+    component: ProjectComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "souls",
+    component: ClientsComponent,
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    redirectTo: "dashboard/analytics",
+    pathMatch: "full",
+  },
+  {
+    path: "",
     component: LayoutComponent,
     canActivate: [authGuard],
     loadChildren: () =>
-      import('./views/views.route').then((mod) => mod.VIEW_ROUTES),
+      import("./views/views.route").then((mod) => mod.VIEW_ROUTES),
   },
   {
-    path: 'auth',
+    path: "auth",
     loadChildren: () =>
-      import('./views/auth/auth.route').then((mod) => mod.AUTH_ROUTES),
+      import("./views/auth/auth.route").then((mod) => mod.AUTH_ROUTES),
   },
   {
-    path: 'not-found',
+    path: "not-found",
     component: Error404Component,
-    data: { title: '404 - Error' },
+    data: { title: "404 - Error" },
   },
   {
-    path: 'error-500',
+    path: "error-500",
     component: Error500Component,
-    data: { title: '500 - Error' },
+    data: { title: "500 - Error" },
   },
   {
-    path: 'maintenance',
+    path: "maintenance",
     component: MaintenanceComponent,
-    data: { title: 'Maintenance' },
+    data: { title: "Maintenance" },
   },
-]
+];
